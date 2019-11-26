50a4b6779bf081b3629c5b3d6f9e16f863900906
Johel Munoz <johelfoos@gmail.com>
2019-11-26 17:25:41 -0600

    Cambios en dashboard

--- WebAsada/Controllers/DashboardController.cs
+++ WebAsada/Controllers/DashboardController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebAsada.ViewModels;
 
 namespace WebAsada.Controllers
 {
     public class DashboardController : Controller
-    {
+    { 
+
         public IActionResult Index()
         {
-            return View();
+
+            var dashboardVM = new DashboardVM() { DashboardReceiptsVM = new DashboardReceiptsVM() { TotalReceipts= 120, TotalReceiptsPaid =50 } };
+            var a = dashboardVM.DashboardReceiptsVM.TotalReceiptsPaidPercentage;
+            var b = dashboardVM.DashboardReceiptsVM.TotalReceiptsPendingPercentage;
+
+            return View(dashboardVM);
         }
     }
 }
---
+++ WebAsada/ViewModels/DashboardVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAsada.ViewModels
+{
+    public class DashboardVM
+    {
+        public DashboardVM()
+        {
+
+        }
+
+        public DashboardReceiptsVM DashboardReceiptsVM { get; set; }
+    }
+
+    public class DashboardReceiptsVM
+    {
+        public int TotalReceiptsPaid { get; set; }
+        public int TotalReceipts { get; set; }
+        public int TotalReceiptsPending => TotalReceipts - TotalReceiptsPaid;
+        public double TotalReceiptsPaidPercentage => (TotalReceiptsPaid / TotalReceipts) * 100;
+        public double TotalReceiptsPendingPercentage => (TotalReceiptsPending / TotalReceipts) * 100;
+    }
+}
